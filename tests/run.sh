41e59737c7b472c9121079f3617eb37e348002e2
MateuszDev96 <33595932+MateuszDev96@users.noreply.github.com>
2025-08-31 22:47:04 +0200

    d

--- tests/run.sh
+++ tests/run.sh
@@ -6,7 +6,7 @@ qemu-riscv64 ./tests/program1/program1.exec
 if [ $? = 0 ]; then
   echo "OK program1"
 else
-  echo "FAIL $0"
+  echo "FAIL program1"
   exit 1
 fi
 
@@ -17,7 +17,18 @@ qemu-riscv64 ./tests/program2/program2.exec
 if [ $? = 42 ]; then
   echo "OK program2"
 else
-  echo "FAIL $0"
+  echo "FAIL program2"
+  exit 1
+fi
+
+./build/minicc ./tests/program3/program3.cwe ./tests/program3/program3.s
+riscv64-linux-gnu-gcc -static -o ./tests/program3/program3.exec ./tests/program3/program3.s
+qemu-riscv64 ./tests/program3/program3.exec
+
+if [ $? = 114 ]; then
+  echo "OK program3"
+else
+  echo "FAIL program3"
   exit 1
 fi
 
