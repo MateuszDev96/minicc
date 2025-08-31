433c4f552c1c8fcbbd09a2b04d70873da171db87
MateuszDev96 <33595932+MateuszDev96@users.noreply.github.com>
2025-08-31 15:29:59 +0200

    s

--- tests/run.sh
+++ tests/run.sh
@@ -1,21 +1,5 @@
-
-# expected=0
-# input="int main() {return 0;}"
-
-# ./build/minicc "$input" > ./build/tmp.s || exit
-./tools/build.sh
 ./build/minicc -c ./tests/program1.cwe
-riscv64-linux-gnu-gcc -static -o ./tests/program1 ./tests/program1.s
-# riscv64-linux-gnu-gcc -static -o ./build ./main.s
-# qemu-riscv64-static ./build/tmp
+riscv64-linux-gnu-gcc -static -o ./main ./main.s
+qemu-riscv64 ./main
 
-# ../build/minicc -c ../main.cwe
-
-# actual="$?"
-
-# if [ "$actual" = "$expected" ]; then
-#   echo "okkk"
-# else
-#   echo "$input => $expected expected, but got $actual"
-#   exit 1
-# fi
+echo -e "\n Exit code: $?"
