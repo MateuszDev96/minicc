2d179f98ee142931b7a9919e304bc37acd9a67af
MateuszDev96 <33595932+MateuszDev96@users.noreply.github.com>
2025-08-31 22:27:53 +0200

    s

--- tests/program1/run.sh
+++ tests/program1/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./build/minicc ./tests/program1/program1.cwe ./tests/program1/program1.s
-riscv64-linux-gnu-gcc -static -o ./tests/program1/program1 ./tests/program1/program1.s
-qemu-riscv64 ./tests/program1/program1
+riscv64-linux-gnu-gcc -static -o ./tests/program1/program1.bin ./tests/program1/program1.s
+qemu-riscv64 ./tests/program1/program1.bin
 
 if [ $? = 0 ]; then
   echo "OK $0"
