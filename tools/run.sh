85ba94576d026ee316622c78c6731ead27d22d68
MateuszDev96 <33595932+MateuszDev96@users.noreply.github.com>
2025-09-06 12:04:18 +0200

    e

--- tools/run.sh
+++ tools/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./tools/build.sh
 ./build/minicc ./main.cwe ./main.s
-riscv64-linux-gnu-gcc -static -o ./main ./main.s
-qemu-riscv64 ./main
+riscv64-linux-gnu-gcc -static -o ./main.exec ./main.s
+qemu-riscv64 ./main.exec
 
 echo -e "\n Exit code: $?"
